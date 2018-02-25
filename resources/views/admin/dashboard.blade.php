cdfde3732a3fca02d5c50c3b5f0bdd27488bb13c
ATarasov <usproinfo@gmail.com>
2018-02-25 20:45:37 +0200

    Create admin dashboard

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,59 @@
+@extends('admin.layouts.app_admin')
+
+@section('content')
+<div class="container">
+	<div class="row">
+		<div class="col-sm-3">
+			<div class="jumbotron">
+				<p>
+					<span class="label label-primary">Cat 1</span>
+				</p>
+			</div>
+		</div>
+		<div class="col-sm-3">
+			<div class="jumbotron">
+				<p>
+					<span class="label label-primary">Cat 2</span>
+				</p>
+			</div>
+		</div>
+		<div class="col-sm-3">
+			<div class="jumbotron">
+				<p>
+					<span class="label label-primary">Cat 3</span>
+				</p>
+			</div>
+		</div>
+		<div class="col-sm-3">
+			<div class="jumbotron">
+				<p>
+					<span class="label label-primary">Cat 4</span>
+				</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-sm-6">
+			<a class="btn btn-block btn-default" href="#">Создать категорию</a> 
+			<a class="list-group-item" href="#">
+				<h4 class="list-group-item-heading">Категория первая</h4>
+				<p class="list-group-item-text">
+					Count of matetials
+				</p>
+			</a> 
+		</div>
+		<div class="col-sm-6">
+			<a class="btn btn-block btn-default" href="#">Create material</a> 
+			<a class="list-group-item" href="#">
+				<h4 class="list-group-item-heading">First material</h4>
+				<p class="list-group-item-text">
+					Category
+				</p>
+			</a> 
+		</div>
+	</div>
+</div>
+@endsection
+
+
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::group(['prefix'=>'admin','namespace'=>'Admin','middleware'=>['auth']], function(){
+	Route::get('/','DashboardController@dashboard')->name('admin.index');
+});
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,3 +21,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
